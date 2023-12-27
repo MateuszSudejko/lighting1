4326f9167fb8a1c81c6bf49056530e059184b275
matsu <mat.sud31@gmail.com>
2023-12-27 15:57:32 +0100

    Add project files.

---
+++ ObjFileParser.cs
@@ -0,0 +1,103 @@
+﻿using OpenTK.Mathematics;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace lighting1
+{
+    public static class ObjFileParser
+    {
+        public static float[] ExtractFaceVertices(string filePath)
+        {
+            List<float> faceVertices = new();
+            List<Vector3> vertices = new();
+            //try
+            //{
+            string[] lines = File.ReadAllLines(filePath);
+
+            foreach (string line in lines)
+            {
+                if (line.StartsWith("v "))
+                {
+                    string[] parts = line.Split(' ');
+
+                    parts = parts.Where(val => val != "").ToArray();
+
+                    vertices.Add(new Vector3(float.Parse(parts[1]), float.Parse(parts[2]), float.Parse(parts[3])));
+                }
+                if (line.StartsWith("f "))
+                {
+                    string[] parts = line.Split(' ');
+
+                    parts = parts.Where(val => val != "").ToArray();
+
+                    float[] normal = CalcNormal(vertices[int.Parse(parts[1]) - 1], vertices[int.Parse(parts[2]) - 1], vertices[int.Parse(parts[3]) - 1]);
+
+                    for(int i = 3; i < parts.Length; i++)
+                    {
+                        faceVertices.Add(vertices[int.Parse(parts[1]) - 1].X);
+                        faceVertices.Add(vertices[int.Parse(parts[1]) - 1].Y);
+                        faceVertices.Add(vertices[int.Parse(parts[1]) - 1].Z);
+                        faceVertices.Add(normal[0]);
+                        faceVertices.Add(normal[1]);
+                        faceVertices.Add(normal[2]);
+
+                        faceVertices.Add(vertices[int.Parse(parts[i-1]) - 1].X);
+                        faceVertices.Add(vertices[int.Parse(parts[i-1]) - 1].Y);
+                        faceVertices.Add(vertices[int.Parse(parts[i-1]) - 1].Z);
+                        faceVertices.Add(normal[0]);
+                        faceVertices.Add(normal[1]);
+                        faceVertices.Add(normal[2]);
+
+                        faceVertices.Add(vertices[int.Parse(parts[i]) - 1].X);
+                        faceVertices.Add(vertices[int.Parse(parts[i]) - 1].Y);
+                        faceVertices.Add(vertices[int.Parse(parts[i]) - 1].Z);
+                        faceVertices.Add(normal[0]);
+                        faceVertices.Add(normal[1]);
+                        faceVertices.Add(normal[2]);
+                    }
+                }
+            }
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine("Error reading the OBJ file: " + ex.Message);
+            //}
+
+            faceVertices.Add(faceVertices.Count);
+
+            return faceVertices.ToArray();
+        }
+
+        public static float[] CalcNormal(Vector3 v1, Vector3 v2, Vector3 v3)
+        {
+            float[] ret = new float[3];
+            float[] vec1 = new float[3], vec2 = new float[3];
+            const int x = 0;
+            const int y = 1;
+            const int z = 2;
+
+            vec1[x] = v1.X - v2.X;
+            vec1[y] = v1.Y - v2.Y;
+            vec1[z] = v1.Z - v2.Z;
+
+            vec2[x] = v2.X - v3.X;
+            vec2[y] = v2.Y - v3.Y;
+            vec2[z] = v2.Z - v3.Z;
+
+            ret[x] = vec1[y] * vec2[z] - vec1[z] * vec2[y];
+            ret[y] = vec1[z] * vec2[x] - vec1[x] * vec2[z];
+            ret[z] = vec1[x] * vec2[y] - vec1[y] * vec2[x];
+
+            float sqr = (float)Math.Sqrt((double)(ret[x] * ret[x] + ret[y] * ret[y] + ret[z] * ret[z]));
+
+            ret[x] = ret[x]/sqr;
+            ret[y] = ret[y] / sqr;
+            ret[z] = ret[z] / sqr;
+
+            return ret;
+        }
+    }
+
+}
---
+++ Texture.cs
@@ -0,0 +1,62 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using System.Drawing;
+using System.Drawing.Imaging;
+using PixelFormat = OpenTK.Graphics.OpenGL4.PixelFormat;
+using StbImageSharp;
+using System.IO;
+
+namespace lighting1
+{
+    public class Texture
+    {
+        public readonly int Handle;
+
+        public static Texture LoadFromFile(string path)
+        {
+            // Generate handle
+            int handle = GL.GenTexture();
+
+            // Bind the handle
+            GL.ActiveTexture(TextureUnit.Texture0);
+            GL.BindTexture(TextureTarget.Texture2D, handle);
+
+            StbImage.stbi_set_flip_vertically_on_load(1);
+
+            using (Stream stream = File.OpenRead(path))
+            {
+                ImageResult image = ImageResult.FromStream(stream, ColorComponents.RedGreenBlueAlpha);
+                GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, image.Width, image.Height, 0, PixelFormat.Rgba, PixelType.UnsignedByte, image.Data);
+            }
+
+            // Now that our texture is loaded, we can set a few settings to affect how the image appears on rendering.
+
+            // First, we set the min and mag filter. These are used for when the texture is scaled down and up, respectively.
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
+
+            // Now, set the wrapping mode. S is for the X axis, and T is for the Y axis.
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)TextureWrapMode.Repeat);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)TextureWrapMode.Repeat);
+
+            // Next, generate mipmaps.
+            GL.GenerateMipmap(GenerateMipmapTarget.Texture2D);
+
+            return new Texture(handle);
+        }
+
+        public Texture(int glHandle)
+        {
+            Handle = glHandle;
+        }
+
+        // Activate texture
+        // Multiple textures can be bound, if your shader needs more than just one.
+        // If you want to do that, use GL.ActiveTexture to set which slot GL.BindTexture binds to.
+        // The OpenGL standard requires that there be at least 16, but there can be more depending on your graphics card.
+        public void Use(TextureUnit unit)
+        {
+            GL.ActiveTexture(unit);
+            GL.BindTexture(TextureTarget.Texture2D, Handle);
+        }
+    }
+}
